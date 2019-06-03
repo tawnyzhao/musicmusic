d5df7033e028b9c52ced9bc9a3639552ee1b5e93
tawnyzhao <41414030+tawnyzhao@users.noreply.github.com>
2019-06-03 14:19:46 -0700

    bugfixes and added class for music

--- musicmusic.py
+++ musicmusic.py
@@ -7,7 +7,7 @@ def clear():
     os.system('clear')
 	
 def reset_lights():
-	GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(18, GPIO.OUT)
     GPIO.setup(15, GPIO.OUT)
@@ -19,38 +19,56 @@ def reset_lights():
     GPIO.setup(7, GPIO.OUT)
 
 def display_amplitude(amplitude):
-if amplitude[i] > 3:
-            GPIO.output(14,GPIO.HIGH)
-        else:
-            GPIO.output(14,GPIO.LOW)
-        if amplitude[i] > 8:
-            GPIO.output(15,GPIO.HIGH)
-        else:
-            GPIO.output(15,GPIO.LOW)
-        if amplitude[i] > 15:
-            GPIO.output(18,GPIO.HIGH)
-        else:
-            GPIO.output(18,GPIO.LOW)
-        if amplitude[i] > 22:
-            GPIO.output(23,GPIO.HIGH)
-        else:
-            GPIO.output(23,GPIO.LOW)
-        if amplitude[i] > 30:
-            GPIO.output(24,GPIO.HIGH)
-        else:
-            GPIO.output(24,GPIO.LOW)
-        if amplitude[i] > 34:
-            GPIO.output(25,GPIO.HIGH)
-        else:
-            GPIO.output(25,GPIO.LOW)
-        if amplitude[i] > 39:
-            GPIO.output(8,GPIO.HIGH)
-        else:
-            GPIO.output(8,GPIO.LOW)
-        if amplitude[i] > 43:
-            GPIO.output(7,GPIO.HIGH)
-        else:
-            GPIO.output(7,GPIO.LOW)
+    if amplitude > 3:
+        GPIO.output(14,GPIO.HIGH)
+    else:
+        GPIO.output(14,GPIO.LOW)
+    if amplitude > 8:
+         GPIO.output(15,GPIO.HIGH)
+    else:
+        GPIO.output(15,GPIO.LOW)
+    if amplitude > 15:
+        GPIO.output(18,GPIO.HIGH)
+    else:
+        GPIO.output(18,GPIO.LOW)
+    if amplitude > 22:
+        GPIO.output(23,GPIO.HIGH)
+    else:
+        GPIO.output(23,GPIO.LOW)
+    if amplitude > 30:
+        GPIO.output(24,GPIO.HIGH)
+    else:
+        GPIO.output(24,GPIO.LOW)
+    if amplitude > 34:
+        GPIO.output(25,GPIO.HIGH)
+    else:
+        GPIO.output(25,GPIO.LOW)
+    if amplitude > 39:
+        GPIO.output(8,GPIO.HIGH)
+    else:
+        GPIO.output(8,GPIO.LOW)
+    if amplitude > 43:
+        GPIO.output(7,GPIO.HIGH)
+    else:
+        GPIO.output(7,GPIO.LOW)
+
+class Music:
+    def __init__(self, filename):
+        self.file_name = file_name
+        self.frame_rate, self.amplitude = read(file_name)
+        self.frame_skip = self.frame_rate/1000.0
+        try:
+            self.amplitude = self.amplitude[:,0] + self.amplitude[:,1]
+        except:
+            pass
+        finally:
+            self.amplitude = self.amplitude[::int(frame_skip)]
+        self.song_total= float(len(self.amplitude)/self.frame_rate)
+        self.max_amplitude = max(amplitude)
+    def play():
+        for i in range(len(self.amplitude) - 2):
+            self.amplitude[i] = float(abs(self.amplitude[i]))/self.max_amplitude*50
+
 def main():
     # Colours for terminal text
     COLOUR = '\033[0;{}m'.format(random.randint(31,37))
@@ -78,6 +96,7 @@ def main():
 	
     song_total = float(len(amplitude)/frame_rate) # song time in seconds
     clear()
+    reset_lights()
 	
 	
     amplitude = amplitude[::int(frame_skip)]
@@ -88,7 +107,7 @@ def main():
 
     max_amplitude = max(amplitude)
     for i in range(len(amplitude)- 2):
-            amplitude[i] = float(abs(amplitude[i]))/max_amplitude*50
+        amplitude[i] = float(abs(amplitude[i]))/max_amplitude*50
             
     pygame.mixer.init(frame_rate)
     pygame.mixer.music.load(file_name)
@@ -100,11 +119,11 @@ def main():
     now = time.time()
 
     for i in range(len(amplitude)):
-		display_amplitude(amplitude)        
+	display_amplitude(amplitude[i])        
 		
         while time.time()< now + (1.0000000000/frame_rate*frame_skip*0.99):
-	    time.sleep(.00000000001)
-	now = time.time()
+            time.sleep(.00000000001)
+        now = time.time()
 	
 if __name__== '__main__':
     main()
